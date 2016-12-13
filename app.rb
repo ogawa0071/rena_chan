fe939bf61d9ebfbc594cdeac5674f5ac522d3be7
Futa Ogawa <ogawa0071@gmail.com>
2016-12-13 09:14:37 +0900

    条件分岐を修正

--- app.rb
+++ app.rb
@@ -30,12 +30,13 @@ post '/callback' do
           }
           client.reply_message(event['replyToken'], message)
         end
-        if event.message['text'] === 'それな'
+        if event.message['text'] === それな
           message = {
             type: 'text',
             text: 'いまのそれな言いたかったー！'
           }
           client.reply_message(event['replyToken'], message)
+        end
       end
     end
   }
